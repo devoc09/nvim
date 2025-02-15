8f19217878339f5fa7b3cb98c821c58297200469
takumi.katase <takumi.katase@devoc.ninja>
2025-02-15 19:56:30 +0900

    change colorscheme to onehalfdark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,21 @@
 return {
-    "devoc09/lflops.nvim",
+    -- "devoc09/lflops.nvim",
+    "sonph/onehalf",
     -- dir = '~/go/src/github.com.devoc09/devoc09/lflops.nvim',
-    config = function()
-      require('lflops').setup({
-        italic = {
-          strings = false,
-          emphasis = false,
-          comments = false,
-          operators = false,
-          folds = false,
-        },
-      })
-      vim.cmd("colorscheme lflops")
+    lazy = false,
+    rtp = 'vim',
+    config = function(plugin)
+      -- require('lflops').setup({
+      --   italic = {
+      --     strings = false,
+      --     emphasis = false,
+      --     comments = false,
+      --     operators = false,
+      --     folds = false,
+      --   },
+      -- })
+      vim.opt.rtp:append(plugin.dir .. "/vim")
+      vim.cmd("colorscheme onehalfdark")
       vim.opt.background = "dark"
       vim.opt.termguicolors = true
     end,
