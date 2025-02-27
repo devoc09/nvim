4d9bf880663ecc3d2f1a18d279ec1cd7cc7b0ef4
Takumi Katase <takumi.katase@devoc.ninja>
2025-02-27 17:24:36 +0900

    feat: add go.nvim & enabled lsp codeaction
    Signed-off-by: Takumi Katase <takumi.katase@devoc.ninja>

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -6,20 +6,22 @@ return {
   },
   {
     'devoc09/go-test.nvim',
+    -- dir = '~/go/src/github.com.devoc09/devoc09/go-test.nvim',
     keys = {
       { '<leader>,', function() require('go-test').go_test_package() end },
       { '<leader>t', function() require('go-test').go_test_all() end },
     },
     ft = "go",
+    enabled = false,
+  },
+  {
+    'ray-x/go.nvim',
+    ft = { "go", "gomod" },
+    event = { "CmdlineEnter" },
+    build = ':lua require("go.install").update_all_sync()',
+    config = function()
+      require('go').setup()
+    end,
     enabled = true,
-  }
-  -- {
-  --   dir = '~/go/src/github.com.devoc09/devoc09/go-test.nvim',
-  --   keys = {
-  --     { '<leader>,', function() require('go-test').go_test_package() end },
-  --     { '<leader>t', function() require('go-test').go_test_all() end },
-  --   },
-  --   ft = "go",
-  --   enabled = true,
-  -- },
+  },
 }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -17,7 +17,7 @@ return {
       buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
       buf_set_keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
       buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-      -- buf_set_keymap("n", "<Leader>k", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+      buf_set_keymap("n", "<Leader>k", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
       buf_set_keymap("n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
       buf_set_keymap("n", "<Leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 
