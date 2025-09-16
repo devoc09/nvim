6e41c72a896bd362aed66ceea20a5892f100d2c4
Takumi Katase <takumi.katase@devoc.ninja>
2025-09-17 00:02:52 +0900

    feat: configure copilot language server
    Signed-off-by: Takumi Katase <takumi.katase@devoc.ninja>

--- lua/config/builtin-lsp.lua
+++ lua/config/builtin-lsp.lua
@@ -57,24 +57,6 @@ vim.lsp.config['lua_ls'] = {
   }
 }
 
--- vim.lsp.config['ts_ls'] = {
---   cmd = { 'typescript-language-server', '--stdio' },
---   root_markers = {
---     'tsconfig.json',
---     'jsconfig.json',
---     'package.json',
---     '.git'
---   },
---   filetypes = {
---     'javascript',
---     'javascriptreact',
---     'javascript.jsx',
---     'typescript',
---     'typescriptreact',
---     'typescript.tsx',
---   },
--- }
-
 vim.lsp.config['tsgo'] = {
   cmd = { 'pnpm', 'exec', 'tsgo', '--lsp', '--stdio' },
   root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock', 'deno.lock', '.git' },
@@ -99,7 +81,99 @@ vim.lsp.config['rust_analyzer'] = {
   },
 }
 
-vim.lsp.enable({ 'gopls', 'lua_ls', 'tsgo', 'rust_analyzer' })
+---@param client vim.lsp.Client
+local function sign_in(bufnr, client)
+  client:request(
+  ---@diagnostic disable-next-line: param-type-mismatch
+    'signIn',
+    vim.empty_dict(),
+    function(err, result)
+      if err then
+        vim.notify(err.message, vim.log.levels.ERROR)
+        return
+      end
+      if result.command then
+        local code = result.userCode
+        local command = result.command
+        vim.fn.setreg('+', code)
+        vim.fn.setreg('*', code)
+        local continue = vim.fn.confirm(
+          'Copied your one-time code to clipboard.\n' .. 'Open the browser to complete the sign-in process?',
+          '&Yes\n&No'
+        )
+        if continue == 1 then
+          client:exec_cmd(command, { bufnr = bufnr }, function(cmd_err, cmd_result)
+            if cmd_err then
+              vim.notify(err.message, vim.log.levels.ERROR)
+              return
+            end
+            if cmd_result.status == 'OK' then
+              vim.notify('Signed in as ' .. cmd_result.user .. '.')
+            end
+          end)
+        end
+      end
+
+      if result.status == 'PromptUserDeviceFlow' then
+        vim.notify('Enter your one-time code ' .. result.userCode .. ' in ' .. result.verificationUri)
+      elseif result.status == 'AlreadySignedIn' then
+        vim.notify('Already signed in as ' .. result.user .. '.')
+      end
+    end
+  )
+end
+
+---@param client vim.lsp.Client
+local function sign_out(_, client)
+  client:request(
+  ---@diagnostic disable-next-line: param-type-mismatch
+    'signOut',
+    vim.empty_dict(),
+    function(err, result)
+      if err then
+        vim.notify(err.message, vim.log.levels.ERROR)
+        return
+      end
+      if result.status == 'NotSignedIn' then
+        vim.notify('Not signed in.')
+      end
+    end
+  )
+end
+
+---@type vim.lsp.Config
+vim.lsp.config['copilot'] = {
+  cmd = {
+    'copilot-language-server',
+    '--stdio',
+  },
+  root_markers = { '.git' },
+  init_options = {
+    editorInfo = {
+      name = 'Neovim',
+      version = tostring(vim.version()),
+    },
+    editorPluginInfo = {
+      name = 'Neovim',
+      version = tostring(vim.version()),
+    },
+  },
+  settings = {
+    telemetry = {
+      telemetryLevel = 'all',
+    },
+  },
+  on_attach = function(client, bufnr)
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspCopilotSignIn', function()
+      sign_in(bufnr, client)
+    end, { desc = 'Sign in Copilot with GitHub' })
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspCopilotSignOut', function()
+      sign_out(bufnr, client)
+    end, { desc = 'Sign out Copilot with GitHub' })
+  end,
+}
+
+vim.lsp.enable({ 'gopls', 'lua_ls', 'tsgo', 'rust_analyzer', 'copilot' })
 
 -- Keymaps of LSP
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -129,6 +203,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
     if client:supports_method('textDocument/codeAction') then
       vim.keymap.set('n', '<Leader>k', vim.lsp.buf.code_action, keyopts)
+    end
+    if client:supports_method('textDocument/inlineCompletion') then
+      vim.keymap.set('i', '<C-j>', vim.lsp.inline_completion.get, keyopts)
     end
     -- if client:supports_method('textDocument/signatureHelp') then
     --   vim.api.nvim_create_autocmd('CursorHoldI', {
